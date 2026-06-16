d97a0810a4a91f4917b39ab3c0d96f1b55f4dc00
Stevenslikibi <likipadoustevens@icloud.com>
2026-06-16 03:50:24 +0200

    Restaurer chemin routes/api

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const path = require('path');
 require('dotenv').config();
 
-const apiRoutes = require('./api');
+const apiRoutes = require('./routes/api');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
